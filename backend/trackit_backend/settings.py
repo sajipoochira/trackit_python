ae67651d8e5cda9b5fbd4fd3095d1f088558d925
sajipoochira <43352568+sajipoochira@users.noreply.github.com>
2025-06-05 17:59:59 +0300

    Docker configuration for frontend and database

--- backend/trackit_backend/settings.py
+++ backend/trackit_backend/settings.py
@@ -35,8 +35,12 @@ WSGI_APPLICATION = 'trackit_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'trackit'),
+        'USER': os.environ.get('POSTGRES_USER', 'trackit'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'trackit'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
