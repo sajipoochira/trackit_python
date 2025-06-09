d1ca833b9da9bb7d519155c61577ec9c951dfe97
sajipoochira <43352568+sajipoochira@users.noreply.github.com>
2025-06-09 17:53:23 +0300

    Fix JSX closing tag error in Layout component

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -44,7 +44,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto">
               {navItems.map((item) => (
-                <li className="nav-item\" key={item.path}>
+                <li className="nav-item" key={item.path}>
                   <Link 
                     className={`nav-link ${location.pathname === item.path ? 'active' : ''}`}
                     to={item.path}
@@ -65,7 +65,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             </button>
           </div>
         </div>
-      </div>
+      </nav>
 
       {/* Main Content */}
       <main className="container py-4">
